683ab01ff777550cd6c997eb4171dd4394d69fc9
Rich Wareham <rjw57@cam.ac.uk>
2013-01-07 14:30:09 +0000

    bump

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@
 
   this.coordIndices = [0, 0];
 
-  this.inputProj = new Proj4js.Proj('EPSG:4236');
-
-  this.outputProj = new Proj4js.Proj('EPSG:4236');
-
   this.outputFilename = 'projected.csv';
 
   $(function() {
     var _this = this;
+    Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+    Proj4js.defs["EPSG:27700"] = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
+    this.inputProj = new Proj4js.Proj('EPSG:4326');
+    this.outputProj = new Proj4js.Proj('EPSG:4326');
     if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
       addAlert('Your browser does not support the HTML5 "File" API and so this web site will not work.');
       return;
